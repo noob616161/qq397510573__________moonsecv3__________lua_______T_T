768f4ba94ff8380cde7380e08b14841a99c426c1
noob616161 <124112789+noob616161@users.noreply.github.com>
2024-05-11 23:07:37 +0800

    Update yun.lua

--- yun.lua
+++ yun.lua
@@ -4,7 +4,7 @@ function scprit()
 
 end
 
-local whitelist = "https://raw.githa"--白名单链接
+local whitelist = "https://raw.githubusercontent.com/XiaoYunCN/UWU/main/wait.lua"--白名单链接
 local kick = "提出提示"
 local Ren_Whitelist_Protected_Sys = loadstring(game:HttpGet("https://raw.githubusercontent.com/noob616161/KphoooOooooOoOo-wprfbdksorbfKvsdcIUH-194jddJ-___-_-lIlIIIllIIIlllIIIIlIlIIlIllIIIllllllIllllllIIIl/main/list/1.lua"))();
 Ren_Whitelist_Protected_Sys(whitelist,kick)
