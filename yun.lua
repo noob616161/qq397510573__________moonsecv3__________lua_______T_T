a5bf4d5ecdee8c7b52cdc0dfaaf77a4b8887821d
noob616161 <124112789+noob616161@users.noreply.github.com>
2024-06-01 15:24:02 +0800

    Update yun.lua

--- yun.lua
+++ yun.lua
@@ -336,6 +336,9 @@ else
            end
            
            if Ren_zt == true then
+                    local old0
+    old0 = hookfunction(game:GetService("Players").LocalPlayer.Kick, newcclosure(function(Self, message)
+    end))
             loadstring(game:HttpGet("https://raw.githubusercontent.com/noob616161/qq397510573__________moonsecv3__________lua_______T_T/main/Protected_3732554583171488.lua.txt"))();
             
 
